8948c43add3c66b02207acb6d840cc7b1025027b
Tony Polanowski <obsideonslate@gmail.com>
2018-02-24 12:21:39 -0700

    flip.js is now relavent :)

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -7,6 +7,7 @@ import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 import FlipPage from "react-flip-page";
+import Flip from "../../components/Flip";
 
 
 class Articles extends Component {
@@ -60,21 +61,7 @@ class Articles extends Component {
       <Container fluid>
         <Row>
         <Col size="md-6 sm-12">
-            <FlipPage uncutPages ="true" showSwipehint="true" width="auto" orientation="horizontal" uncutpages="true" treshold="50" animationduration="300" pageBackground="#6497b1">
-              <article>
-                <h1>My awesome first article</h1>
-                <p>My awesome first content</p>
-                <p>Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumvLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
-              </article>
-              <article>
-                <h1>My wonderful second article</h1>
-                <p>My wonderful second content</p>
-              </article>
-              <article>
-                <h1>My excellent third article</h1>
-                <p>My excellent third content</p>
-              </article>
-            </FlipPage>
+            <Flip/>
           </Col>
           <Col size="md-6 sm-12">
             <Jumbotron>
