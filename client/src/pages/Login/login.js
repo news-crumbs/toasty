0a2c00132bd42ef32c8d608c98a3b449e4d9739e
Andrew Ferrer <andb.ferrer@gmail.com>
2018-03-10 17:33:41 -0700

    pushing some functional and nonfunctional login.js changes

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -17,49 +17,81 @@ class Login extends Component {
     newNom:"",
     newPass:""
   };
-  // When this component mounts, grab the article with the _id of this.props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-  componentDidMount() {
-    // API.getArticle(this.props.match.params.id)
-    //   .then(res => this.setState({ article: res.data }))
-    //   .catch(err => console.log(err));
+  constructor(props) {
+    super(props)
+    this.validateInput = this.validateInput.bind(this)
+    this.validateDB = this.validateDB.bind(this)
   }
+  
+    validateInput(newUser, cb) {
+      // if( newUser.name === ""){
+      //     $("#noToast").text(`Please enter a username.`);
+      //     $("#name").val("");
+      // } else if ( newUser.password === ""){
+      //     $("#noToast").text(`Please enter a password.`);
+      //     $("#pass").val("");
+      // } else{
+      //     cb(newUser);
+      // }
+      console.log('validating input');
+    };
 
-   handleFormSubmit = event => {
-    event.preventDefault();
-    console.log(this.state);
-    var apiBaseUrl = "http://localhost:3000/api/users";
-    var payload={
-      "nom":this.state.newNom,
-      "password":this.state.newPass
-      };
-      axios.post(apiBaseUrl, payload)
-      .then(function (response) {
-      console.log(response);
-      if(response.data.code == 200){
-      console.log("Login successfull");
-      }
-      else if(response.data.code == 204){
-      console.log("Username password do not match");
-      alert("username password do not match")
-      }
-      else{
-      console.log("Username does not exists");
-      alert("Username does not exist");
-      }
-      })
-      .catch(function (error) {
-      console.log(error);
+    validateDB () {
+      // $.get("/api/users/" + newUser.name, function(data) {
+      //     if (data) {
+      //     console.log("Here's the response from the get with the user's name", data);
+      //     $("#name").val("");
+      //     $("#pass").val("");
+      //     $("#noToast").text(`Sorry, ${newUser.name} is taken.`);
+      //     } else {
+      //         post(newUser)
+      //     }
+      // });
+      console.log('validating DB');
+
+    };
+
+    handleLogin = event => {
+      event.preventDefault();
+      console.log('checking DB for user');
+      validateInput(this.state, validateDB);
+    };
+
+    handleRegistration = event => {
+      event.preventDefault();
+      console.log(this.state);
+      var apiBaseUrl = "http://localhost:3000/api/users";
+      var payload={
+        "nom":this.state.newNom,
+        "password":this.state.newPass
+        };
+        axios.post(apiBaseUrl, payload)
+          .then(function (response) {
+          console.log(response);
+          if(response.data.code == 200){
+          console.log("Login successfull");
+          }
+          else if(response.data.code == 204){
+          console.log("Username password do not match");
+          alert("username password do not match")
+          }
+          else{
+          console.log("Username does not exists");
+          alert("Username does not exist");
+          }
+          })
+          .catch(function (error) {
+          console.log(error);
+          });
+    };
+
+    handleInputChange = event => {
+      console.log(event.target.name);
+      const { name, value } = event.target;
+      this.setState({
+        [name]: value
       });
-      }
-
-      handleInputChange = event => {
-        console.log(event.target.name);
-        const { name, value } = event.target;
-        this.setState({
-          [name]: value
-        });
-      };
+    };
   //   axios({
   //     method:'post',
   //     user: 
@@ -78,38 +110,32 @@ class Login extends Component {
       <Container fluid>
         <Row>
           <Col size="md-12">
-          <Col size="md-1">
-          </Col>
-              <Col size="md-4">
-                <div className="login-bkg">
-                  <form className="form">
+              <Col size="md-5">
+                <div class="login-bkg">
+                  <form class="form">
                   <Col size="md-1">
                 </Col>
-                    <p className="login-title">Current Users</p>
-                    <label className="sr-only" htmlFor="inlineFormInput">Name</label>
-                    <input type="text" value= {this.state.nom} name="nom" onChange= {this.handleInputChange} className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Name"></input>
+                    <p class="login-title">Current Users</p>
+                    <label class="sr-only" for="inlineFormInput">Name</label>
+                    <input type="text" value= {this.state.nom} name="nom" onChange= {this.handleInputChange} class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Name"></input>
                     
-                    <label className="sr-only" htmlFor="inlineFormInputGroup">Password</label>
-                    <input type="text" value= {this.state.password} onChange= {this.handleInputChange} name="password" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputGroup" placeholder="Password"></input>
-                    <button type="submit" onClick={this.handleFormSubmit} className="btn btn-primary">Submit</button>
+                    <label class="sr-only" for="inlineFormInputGroup">Password</label>
+                    <input type="text" value= {this.state.password} onChange= {this.handleInputChange} name="password" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputGroup" placeholder="Password"></input>
+                    <button type="submit" onClick={this.handleLogin} class="btn btn-primary">Submit</button>
                   </form>
                 </div>
               </Col>
-              <Col size="md-2">
-              </Col>
-              <Col size="md-4">
-              <div className="login-bkg">
-                  <form className="form">
-                  <p className="login-title">New Users</p>
-                    <label className="sr-only" htmlFor="inlineFormInput">Name</label>
-                    <input type="text" value={this.state.newNom} onChange={this.handleInputChange} name="newNom" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Name"></input>
-                    <label className="sr-only" htmlFor="inlineFormInputGroup">Password</label>
-                    <input type="text" value={this.state.newPass} onChange={this.handleInputChange} name="newPass" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputGroup" placeholder="Password"></input>
-                    <button type="submit" onClick={this.handleFormSubmit} className="btn btn-primary">Register</button>
+              <Col size="md-5">
+              <div class="login-bkg">
+                  <form class="form">
+                  <p class="login-title">New Users</p>
+                    <label class="sr-only" for="inlineFormInput">Name</label>
+                    <input type="text" value={this.state.newNom} onChange={this.handleInputChange} name="newNom" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Name"></input>
+                    <label class="sr-only" for="inlineFormInputGroup">Password</label>
+                    <input type="text" value={this.state.newPass} onChange={this.handleInputChange} name="newPass" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputGroup" placeholder="Password"></input>
+                    <button type="submit" onClick={this.handleRegistration} class="btn btn-primary">Register</button>
                   </form>
                 </div>
-              </Col>
-              <Col size="md-1">
               </Col>
           </Col>
           <Col size="md-1">
@@ -124,7 +150,7 @@ class Login extends Component {
         <Col size="md-5">
           </Col>
           <Col size="md-2">
-            <p className="grn-btn"><Link to="./Articles">← Back to Articles</Link></p>
+            <p className="grn-btn"><Link to="/">← Back to Articles</Link></p>
           </Col>
           <Col size="md-5">
           </Col>
